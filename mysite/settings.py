b1a14944a58116db9f38a6128284d2dbcc6d2237
960h <coffee.310h@gmail.com>
2019-05-24 22:42:37 +0900

    Modified templates to display posts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,5 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Additional locations of static files
+ROOT_PATH = os.path.dirname(__file__)
+STATICFILES_DIRS = (
+    [os.path.join(ROOT_PATH, 'static')]
+)
